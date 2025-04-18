5bc86eee27f493f9dd5ccf562026be1567f4e7c2
RohitKonge <https://github.com/RohitKonge>
2025-04-18 11:06:35 +0530

    2nd commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import yt_dlp
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS if frontend runs separately
@@ -35,4 +36,5 @@ def get_download_link():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
